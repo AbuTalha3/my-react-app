44c6fdbf12881e823d26cbbd08900721d0f5a7c3
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-10-10 09:30:09 +0500

    Configure the repo and packages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          You can Access my GitHUb Profile with the link below.
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/AbuTalha3"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          My Profile on GitHub
-        </a>
-      </header>
+      My Meal App
     </div>
   );
 }
