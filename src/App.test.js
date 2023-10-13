5d6e70cb2f9074782a875a2e2757680fdd15b8bb
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-10-14 01:00:10 +0500

    Remove linter errors

--- src/App.test.js
+++ src/App.test.js
@@ -6,7 +6,6 @@ import store from './Redux/store';
 import App from './App';
 import '@testing-library/jest-dom';
 
-
 describe('tests my Food app', () => {
   test('renders App component', () => {
     render(
@@ -16,5 +15,5 @@ describe('tests my Food app', () => {
         </Router>
       </Provider>,
     );
-  });  
+  });
 });
